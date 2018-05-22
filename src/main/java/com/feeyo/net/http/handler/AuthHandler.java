01a7972044983c39948863b1670e8193d3ad0197
zhuam <zhuam@foxmail.com>
2018-05-22 18:40:22 +0800

    add login.vm

--- src/main/java/com/feeyo/net/http/handler/AuthHandler.java
+++ src/main/java/com/feeyo/net/http/handler/AuthHandler.java
@@ -1,14 +1,26 @@
 package com.feeyo.net.http.handler;
 
+import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Map;
 
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBuffers;
+import org.jboss.netty.channel.ChannelFuture;
 import org.jboss.netty.channel.ChannelHandlerContext;
 import org.jboss.netty.channel.MessageEvent;
 import org.jboss.netty.handler.codec.http.DefaultHttpRequest;
+import org.jboss.netty.handler.codec.http.DefaultHttpResponse;
+import org.jboss.netty.handler.codec.http.HttpHeaders;
 import org.jboss.netty.handler.codec.http.HttpRequest;
+import org.jboss.netty.handler.codec.http.HttpResponse;
+import org.jboss.netty.handler.codec.http.HttpResponseStatus;
+import org.jboss.netty.handler.codec.http.HttpVersion;
 import org.jboss.netty.handler.codec.http.QueryStringDecoder;
 
+import com.feeyo.util.velocity.VelocityBuilder;
+
+// 
 public class AuthHandler implements IRequestHandler {
 
 	@Override
@@ -57,6 +69,21 @@ public class AuthHandler implements IRequestHandler {
 	    			
 	    		} else if ( "PAGE".equalsIgnoreCase( action ) ) {
 	    			
+	    			
+	    			VelocityBuilder velocityBuilder = new VelocityBuilder();
+	    			String htmlText = velocityBuilder.generate("login.vm", "UTF8", null);
+	    			
+	    			HttpResponse response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
+	    			response.headers().add(HttpHeaders.Names.CONTENT_LENGTH, htmlText.length());
+	    			response.headers().set(HttpHeaders.Names.CONTENT_TYPE, "text/html; charset=UTF-8");
+
+	    			ChannelBuffer buffer = ChannelBuffers.copiedBuffer(htmlText, Charset.defaultCharset());// CharsetUtil.UTF_8);
+	    			response.setContent(buffer);
+	    			
+	    			ChannelFuture channelFuture = ctx.getChannel().write(response);
+	    			if (channelFuture.isSuccess()) {
+	    				channelFuture.getChannel().close();
+	    			}
 	    		}
 	    	}
 	    	
